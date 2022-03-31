4368b9d30d1cb8be261fdcb3ef9136d76b9c35e9
yg <sorisori01@naver.com>
2022-03-31 22:26:30 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,23 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import VideoList from './components/video_list/video_list';
 
-function app() {
-  return <h1>Hello :)</h1>
+function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAKfYrJpDJ_-7ZKDV-lJLvunRfh9XQEvtQ", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+
+  },[])
+  return <VideoList videos={videos}/>
 }
 
-export default app;
+export default App;
